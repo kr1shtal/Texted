cab5018b9a3d3f62d4e93af3bd8071d78dcdaa73
kr1shtal <krishtalforkato@gmail.com>
2024-01-12 20:24:42 +0200

    Turn off all output processing

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ void enableRawMode()
 
     struct termios raw = term;
     raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_oflag &= ~(OPOST);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -32,11 +33,11 @@ int main(void)
     {
         if (iscntrl(c))
         {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         }
         else
         {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
     }
     return(0);
